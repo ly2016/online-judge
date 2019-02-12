ebb5f4f7bd8a0c1f03e238f62f07e6d9ad3dc25d
ly2016 <502052807@qq.com>
2019-02-12 19:18:06 +0800

    3

---
+++ src/main/java/com/ly/oj/leetcode/No_3_无重复字符的最长子串.java
@@ -0,0 +1,30 @@
+package com.ly.oj.leetcode;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class No_3_无重复字符的最长子串 {
+
+    public static void main(String args[]){
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        char[] arr1 = s.toCharArray();
+        Set<Character> child = new LinkedHashSet<>();
+        int max = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = i; j < arr1.length; j++) {
+                int size = child.size();
+                child.add(arr1[j]);
+                if (child.size() == size) {
+                    break;
+                }
+            }
+            max = max > child.size() ? max : child.size();
+            child.clear();
+        }
+        return max;
+    }
+}
+
