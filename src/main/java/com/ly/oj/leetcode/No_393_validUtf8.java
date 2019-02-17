bf1d5f0b2ed3d979f980825138075ed9ba6be06b
ly2019 <hehe@163.com>
2019-02-17 18:31:02 +0800

    393

--- src/main/java/com/ly/oj/leetcode/No_393_validUtf8.java
+++ src/main/java/com/ly/oj/leetcode/No_393_validUtf8.java
@@ -2,7 +2,33 @@ package com.ly.oj.leetcode;
 
 public class No_393_validUtf8 {
 
-    public boolean validUtf8(int[] data) {
-        return true;
+    public static void main(String args[]) {
+        System.err.println(validUtf8(new int[]{197, 130, 1}));
+    }
+
+    public static boolean validUtf8(int[] data) {
+        if (data == null || data.length == 0) {
+            return false;
+        }
+        int cnt = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (cnt == 0) {
+                if (data[i] >> 5 == 0b110) {
+                    cnt = 1;
+                } else if (data[i] >> 4 == 0b1110) {
+                    cnt = 2;
+                } else if (data[i] >> 3 == 0b11110) {
+                    cnt = 3;
+                } else if (data[i] >> 7 == 1) { /*首字符为1XXX XXXX 是错误值，但0XXX XXXX可表示单个字符*/
+                    return false;
+                }
+            } else {
+                if (data[i] >> 6 != 0b10) {
+                    return false;
+                }
+                cnt--;
+            }
+        }
+        return cnt == 0;
     }
 }
