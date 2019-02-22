7dcf407d3faff8a661c351daecb4023232de3451
ly2016 <502052807@qq.com>
2019-02-22 21:06:29 +0800

    56

---
+++ src/main/java/com/ly/oj/leetcode/No_56_merge.java
@@ -0,0 +1,90 @@
+package com.ly.oj.leetcode;
+
+import com.ly.oj.leetcode.structure.Interval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_56_merge {
+
+    public static void main(String[] args) {
+        List<Interval> list = new ArrayList<Interval>();
+
+        Interval l1 = new Interval(1, 4);
+        Interval l2 = new Interval(0, 4);
+        //Interval l3 = new Interval(4, 6);
+        list.add(l1);
+        list.add(l2);
+        //list.add(l3);
+        System.err.println(merge2(list));
+    }
+
+    public static List<Interval> merge(List<Interval> intervals) {
+        Interval first;
+        Interval second;
+        List<Interval> intervalsReturn = new ArrayList<>();
+        for (int i = 0; i < intervals.size(); i++) {
+            first = intervals.get(i);
+            int j = i + 1;
+            for (; j < intervals.size(); j++) {
+                second = intervals.get(j);
+                if (first.start == second.start) {
+                    second.end = Math.max(first.end, second.end);
+                    break;
+                } else if (first.start < second.start) {
+                    if (first.end >= second.start) {
+                        second.start = first.start;
+                        second.end = Math.max(first.end, second.end);
+                        break;
+                    }
+                } else if (first.start > second.start) {
+                    if (first.end > second.end) {
+                        second.end = first.end;
+                        break;
+                    }
+                }
+
+            }
+            if (j == intervals.size()) {
+                intervalsReturn.add(intervals.get(i));
+            }
+        }
+        return intervalsReturn;
+    }
+
+    public static List<Interval> merge2(List<Interval> intervals) {
+        List<Interval> list = new ArrayList<Interval>();
+        Interval L = new Interval();//一层循环保存对象
+        Interval R = new Interval();//二层循环保存对象
+        for (int i = 0; i < intervals.size(); i++) {
+            L = intervals.get(i);
+            int j = i + 1;
+            for (; j < intervals.size(); j++) {
+                R = intervals.get(j);
+                if (R.start == L.start) {
+                    intervals.set(j, new Interval(L.start, Math.max(L.end, R.end)));
+                    break;
+                } else if (L.start < R.start) {
+                    if (L.end >= R.start) {
+                        Interval inter = new Interval(L.start, Math.max(L.end, R.end));
+                        intervals.set(j, inter);
+                        break;
+                    }
+
+                } else {
+                    if (R.end >= L.start) {
+                        Interval inter = new Interval(R.start, Math.max(L.end, R.end));
+                        intervals.set(j, inter);
+                        break;
+                    }
+                }
+            }
+
+            if (j == intervals.size()) {
+                list.add(intervals.get(i));
+            }
+        }
+        return list;
+    }
+
+}
---
+++ src/main/java/com/ly/oj/leetcode/structure/Interval.java
@@ -0,0 +1,17 @@
+package com.ly.oj.leetcode.structure;
+
+public class Interval {
+
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
